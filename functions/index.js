2d2d5435216ce4fb4f95130f39e0984438390d80
seohoyoon93 <ansndmin1@gmail.com>
2019-03-21 07:16:57 +0900

    Get candle data from upbit api and put it in the db
    Signed-off-by: seohoyoon93 <ansndmin1@gmail.com>

--- functions/index.js
+++ functions/index.js
@@ -4,6 +4,21 @@ const request = require("request");
 
 admin.initializeApp();
 
+Date.prototype.getUnixTime = function() {
+  return (this.getTime() / 1000) | 0;
+};
+
+Array.prototype.delayedForEach = function(callback, timeout, thisArg) {
+  var i = 0,
+    l = this.length,
+    self = this,
+    caller = function() {
+      callback.call(thisArg || self, self[i], i, self);
+      ++i < l && setTimeout(caller, timeout);
+    };
+  caller();
+};
+
 exports.setUpbitMarkets = functions.https.onRequest((req, res) => {
   request(
     {
@@ -32,14 +47,18 @@ exports.setUpbitMarkets = functions.https.onRequest((req, res) => {
         })
         .then(() => {
           console.log("Successfully updated upbit!");
+          res.send("Done!");
         })
         .catch(err => {
           console.error("Error updating upbit");
+          res.status(500).send(err);
         });
     }
   );
 });
 
+//TODO: this function should finish when the last base data has received.
+//      currently, it does not finish but goes on forever
 exports.setUpbitMarketData = functions.https.onRequest((req, res) => {
   admin
     .firestore()
@@ -47,7 +66,90 @@ exports.setUpbitMarketData = functions.https.onRequest((req, res) => {
     .get()
     .then(snapshot => {
       const data = snapshot.data();
-      res.send(data.bases);
+      data.bases.delayedForEach(
+        (base, index, array) => {
+          console.log(base, ", ", new Date());
+          const options = {
+            method: "GET",
+            url: "https://api.upbit.com/v1/candles/minutes/1",
+            qs: { market: `KRW-${base}`, count: "2" }
+          };
+
+          request(options, (err, response, result) => {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            const obj = JSON.parse(result);
+            console.log(obj);
+            const currentData = obj[0];
+            const lastData = obj[1];
+
+            //currentData saving
+            const currentStartTime =
+              new Date(currentData.candle_date_time_utc).getUnixTime() * 1000;
+            //lastData to update
+            const lastStartTime =
+              new Date(lastData.candle_date_time_utc).getUnixTime() * 1000;
+
+            admin
+              .firestore()
+              .doc(
+                `exchanges/upbit/KRW-${base}/${
+                  currentData.candle_date_time_utc
+                }`
+              )
+              .set({
+                startTime: currentStartTime,
+                endTime: currentData.timestamp,
+                price: currentData.trade_price,
+                volume: currentData.candle_acc_trade_price
+              })
+              .then(() => {
+                console.log(
+                  "New data updated for KRW-",
+                  base,
+                  " market of Upbit"
+                );
+              })
+              .catch(err => {
+                console.error(
+                  "Error updating new KRW-",
+                  base,
+                  " market data for Upbit"
+                );
+              });
+
+            admin
+              .firestore()
+              .doc(
+                `exchanges/upbit/KRW-${base}/${lastData.candle_date_time_utc}`
+              )
+              .set({
+                startTime: lastStartTime,
+                endTime: lastData.timestamp,
+                price: lastData.trade_price,
+                volume: lastData.candle_acc_trade_price
+              })
+              .then(() => {
+                console.log(
+                  "Last data updated for KRW-",
+                  base,
+                  " market of Upbit"
+                );
+              })
+              .catch(err => {
+                console.error(
+                  "Error updating last KRW-",
+                  base,
+                  " market data for Upbit"
+                );
+              });
+          });
+        },
+        50,
+        null
+      );
     })
     .catch(err => {
       console.log(err);
