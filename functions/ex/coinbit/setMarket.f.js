3503ebf12a0b7c0f511fd21f85889f7e1ad76544
seohoyoon93 <ansndmin1@gmail.com>
2019-03-29 04:52:03 +0900

    Add bitsonic, coinbit set market functions
    Signed-off-by: seohoyoon93 <ansndmin1@gmail.com>

---
+++ functions/ex/coinbit/setMarket.f.js
@@ -0,0 +1,62 @@
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const config = functions.config().firebase;
+try {
+  admin.initializeApp(config);
+} catch (e) {
+  console.log(e);
+}
+
+exports = module.exports = functions.https.onRequest((req, res) => {
+  const bases = [
+    "DEX",
+    "DXR",
+    "DXG",
+    "BLINK",
+    "BTC",
+    "ETH",
+    "BCH",
+    "BSV",
+    "LTC",
+    "ETC",
+    "QTUM",
+    "NEXO",
+    "XTX",
+    "GRS",
+    "LRC",
+    "THETA",
+    "ARDR",
+    "NXT",
+    "IGNIS",
+    "ELF",
+    "ZRX",
+    "KIN",
+    "APIS",
+    "ROMTV",
+    "NET",
+    "MXM",
+    "ENJ",
+    "R",
+    "PAX",
+    "TUSD",
+    "BTG",
+    "SNT",
+    "OMG"
+  ];
+  admin
+    .firestore()
+    .doc("exchanges/coinbit")
+    .set({
+      name: "Coinbit",
+      link: "https://www.coinbit.co.kr",
+      bases: bases
+    })
+    .then(() => {
+      console.log("Successfully updated coinbit!");
+      res.send("Done");
+    })
+    .catch(err => {
+      console.error("Error updating coinbit");
+      res.status(500).send(err);
+    });
+});
